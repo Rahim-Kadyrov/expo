01b267642f5deb33321fcb1bd6607cd84f647f5b
Рахим <114479886+Rahim-Kadyrov@users.noreply.github.com>
2022-09-27 16:38:25 +0300

    Expo

---
+++ FR/javaS/script.js
@@ -0,0 +1,13 @@
+// Правильные переменные!
+alert('Ассаламу Алейкум')
+alert('Алисхан')
+let message;
+
+message = 'Hello';
+alert(message)
+
+let $ = 7
+let r = 6
+alert ($ + $ * r)
+// Неправильные переменные! let 1a; let 75a;
+
